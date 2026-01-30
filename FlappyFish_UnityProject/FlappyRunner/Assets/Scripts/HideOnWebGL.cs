e32a832492595658070bc9f16f3bd694ed7ac2e8
Annahexe <119667850+Annahexe@users.noreply.github.com>
2026-01-30 01:11:24 +0100

    mobile optimization

---
+++ FlappyFish_UnityProject/FlappyRunner/Assets/Scripts/HideOnWebGL.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class HideOnWebGL : MonoBehaviour
+{
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
